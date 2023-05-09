d9c87a865ed3455e37f0af045f523b33dbe05fc9
minhluan306 <minhluan306288@gmail.com>
2023-05-09 22:15:12 +0700

    update context

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,11 +4,15 @@ import { createContext, useState } from "react";
 const AppContext = createContext()
 export function AppProvider({children}){
    const [theme, setTheme] = useState({theme:'dark', layout: 'harizontal'})
+   const [cart, setCart] = useState([])
+   const updateCart = (product) => {
+      setCart(pr => [...pr,{product,quatity: 1}] )
+   }
    
    return(
-      <AppContext.Provider value={{theme,setTheme}} >
+      <AppContext.Provider value={{theme,cart,updateCart,setTheme}} >
          {children}
       </AppContext.Provider> 
    )
 }
-export default AppContext
+export default AppContext 
